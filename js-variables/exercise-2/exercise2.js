789d4c3cad6ca8ee3ce4aed7cf2c577b5f10a108
Janaina Voigt <jv.de@icloud.com>
2024-04-10 15:18:51 +0200

    Added challenge js-basics

---
+++ js-variables/exercise-2/exercise2.js
@@ -0,0 +1,8 @@
+let mealCost = 26.405;
+let mealTip = 0.25;
+let tipAmount = mealCost * mealTip;
+
+console.log("The total cost of your bill is: ", tipAmount);
+
+let totalCost = tipAmount + mealCost;
+console.log("The total cost of your bill is: ", totalCost);
