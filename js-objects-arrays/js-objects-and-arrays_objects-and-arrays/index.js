5345d986707670bf7dd2af3fb18d32e17fc6a95b
janavoigt <150524717+janavoigt@users.noreply.github.com>
2024-04-12 13:19:32 +0200

    Revert "Added result Objects-and-Arrays"

--- js-objects-arrays/js-objects-and-arrays_objects-and-arrays/index.js
+++ js-objects-arrays/js-objects-and-arrays_objects-and-arrays/index.js
@@ -50,31 +50,27 @@ const people = [
 // EXERCISE 1
 // Change the value of the variable nameOfFirstPerson to equal the name of the first person.
 
-const nameOfFirstPerson = people[0].name;
-console.log(nameOfFirstPerson);
+const nameOfFirstPerson = "change me";
 
 // EXERCISE 2
 // Change the value of the variable homersOtherFriends to equal the other friends of Homer.
 
-const homersOtherFriends = people[0]["other friends"];
-console.log(homersOtherFriends);
+const homersOtherFriends = "change me";
 
 // EXERCISE 3
 // Change the value of the variable ageOfThirdPerson to equal the age of the third person.
 
-const ageOfThirdPerson = people[2].age;
-console.log(ageOfThirdPerson);
+const ageOfThirdPerson = "change me";
 
 // EXERCISE 4
 // Change the value of the variable mothersNameOfSecondPerson to equal the mothers name of the second person.
 
-const mothersNameOfSecondPerson = people[1].parents.mothersName;
-console.log(mothersNameOfSecondPerson);
+const mothersNameOfSecondPerson = "change me";
+
 // EXERCISE 5
 // Change the value of the variable animaltypeOfLisasPet to equal the type of animal of Lisas pet.
 
-const animaltypeOfLisasPet = people[3].pet.animal;
-console.log(animaltypeOfLisasPet);
+const animaltypeOfLisasPet = "change me";
 
 export {
   nameOfFirstPerson,
