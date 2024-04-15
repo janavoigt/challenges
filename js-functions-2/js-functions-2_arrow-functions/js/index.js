a1421b44f4ea8081fba3daebaa7b44deea23159e
Janaina Voigt <jv.de@icloud.com>
2024-04-15 12:25:08 +0200

    Added the result

--- js-functions-2/js-functions-2_arrow-functions/js/index.js
+++ js-functions-2/js-functions-2_arrow-functions/js/index.js
@@ -10,18 +10,23 @@ function getCurrentHour() {
   const currentHour = now.getHours();
   if (currentHour === 0) {
     return "12am";
-  } else if (currentHour === 12) {
-    return "12pm";
-  } else if (currentHour <= 11) {
-    return currentHour + "am";
-  } else {
-    return currentHour - 12 + "pm";
   }
+  if (currentHour === 12) {
+    return "12pm";
+  }
+  if (currentHour <= 11) {
+    return currentHour + "am";
+  }
+  return currentHour - 12 + "pm";
 }
+
+const getVectorLength = (x, y, z) => (x ** 2 + y ** 2 + z ** 2) ** 0.5;
 
 function getVectorLength(x, y, z) {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
 }
+
+const cleanInput = (string) => string.toLowerCase().trim();
 
 function cleanInput(string) {
   return string.toLowerCase().trim();
@@ -45,4 +50,12 @@ const isOddAndSmall = (number) => {
 
 const add3 = (a, b, c) => a + b + c;
 
+function add3(a, b, c) {
+  return a + b + c;
+}
+
 const repeat10 = (string) => string.repeat(10);
+
+function repeat10(string) {
+  return string.repeat(10);
+}
