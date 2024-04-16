e7454dc380858ef03b3cd7592293e34824fa7d61
Janaina Voigt <jv.de@icloud.com>
2024-04-16 16:04:40 +0200

    Added the result challenge handle-forms

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+
+  const ageBadnessSum = parseInt(data.age + data.badness);
+
+  console.log(`The age-badness-sum of ${data.firstName} is ${ageBadnessSum}"`);
+  event.target.elements.firstName.focus();
+  event.target.reset();
+});
