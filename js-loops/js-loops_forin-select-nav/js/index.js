6ae149ac904c9dfd43e3ee646129e7228384b432
Janaina Voigt <jv.de@icloud.com>
2024-04-17 20:26:39 +0200

    Added the correct result of the challenge select-and-navigation

--- js-loops/js-loops_forin-select-nav/js/index.js
+++ js-loops/js-loops_forin-select-nav/js/index.js
@@ -24,6 +24,7 @@ main.append(select);
 for (const key in languages) {
   let option = document.createElement("option");
   option.textContent = languages[key];
+  option.value = key;
   select.append(option);
 }
 
