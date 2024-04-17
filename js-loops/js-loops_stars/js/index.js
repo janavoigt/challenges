93eb476de67d37ab6d7bccdf2bda713321024dae
Janaina Voigt <jv.de@icloud.com>
2024-04-17 19:46:21 +0200

    Added result challenge stars

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,27 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  for (let i = 1; i <= 5; i++) {
+    const stars = document.createElement("img");
 
-  //--^-- your code here --^--
+    if (filledStars >= i) {
+      stars.setAttribute("src", "./assets/star-filled.svg");
+      stars.setAttribute("alt", "stars-foto");
+    } else {
+      stars.setAttribute("src", "./assets/star-empty.svg");
+      stars.setAttribute("alt", "stars-foto");
+    }
+    stars.addEventListener("click", () => {
+      renderStars(i);
+    });
+    starContainer.append(stars);
+  }
 }
+//--^-- your code here --^--
 
-renderStars();
+renderStars(0);
