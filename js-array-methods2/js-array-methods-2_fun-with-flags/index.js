899df4275c91049fe21291c0a16b3bd186867cb7
Janaina Voigt <jv.de@icloud.com>
2024-04-24 17:01:36 +0200

    Added the 2 part of the challenge fun-with-flags

--- js-array-methods2/js-array-methods-2_fun-with-flags/index.js
+++ js-array-methods2/js-array-methods-2_fun-with-flags/index.js
@@ -4,7 +4,7 @@ import { Country } from "./components/Country/Country.js";
 const container = document.querySelector('[data-js="card-container"]');
 const queryInput = document.querySelector('[data-js="query-input"]');
 
-queryInput.addEventListener("input", (event) => {
+/*queryInput.addEventListener("input", (event) => {
   container.innerHTML = "";
 
   const searchString = event.target.value;
@@ -16,6 +16,22 @@ queryInput.addEventListener("input", (event) => {
 
   if (foundCountry) {
     const countryElement = Country(foundCountry);
+    container.append(countryElement);
+  }
+});*/
+
+queryInput.addEventListener("input", (event) => {
+  container.innerHTML = "";
+
+  const searchString = event.target.value;
+
+  let result = countries.filter((count) => count.name.startsWith(searchString));
+  console.log(searchString);
+
+  let foundCountries = result;
+
+  for (const countName of foundCountries) {
+    const countryElement = Country(countName);
     container.append(countryElement);
   }
 });
