023bb3c1fba77aff752b1a28f92630407e5a6ff1
Janaina Voigt <jv.de@icloud.com>
2024-04-26 13:42:28 +0200

    Added the result final

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -11,21 +11,17 @@ const url = "https://swapi.py4e.com/api/people";
   return data;
 }*/
 //fetchData();
-
 async function fetchData() {
   try {
+    const url = "https://swapi.py4e.com/api/people";
     const response = await fetch(url);
-
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      console.error("Bad Response");
-    }
+    const data = await response.json();
+    console.log(data);
+    console.log(response);
+    console.log(data.results[2].eye_color);
+    return data;
   } catch (error) {
     console.error("An Error occurred");
   }
-  console.log(data);
 }
-
 fetchData();
