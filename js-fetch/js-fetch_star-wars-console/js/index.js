8547c7ac27fcfbaaf9be5f654be2cf3bde1901d6
Janaina Voigt <jv.de@icloud.com>
2024-04-26 12:31:16 +0200

    Added the 2 task of the challenge

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,15 +2,30 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-async function fetchData() {
+/*async function fetchData() {
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
   console.log(data.results);
   console.log(data.results[2].eye_color);
   return data;
-}
+}*/
+//fetchData();
 
-console.log();
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      console.error("Bad Response");
+    }
+  } catch (error) {
+    console.error("An Error occurred");
+  }
+  console.log(data);
+}
 
 fetchData();
