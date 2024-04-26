6526ac95c5ea2e5052ed077075186b4a22a16f16
Janaina Voigt <jv.de@icloud.com>
2024-04-26 12:15:16 +0200

    Added the 1 task pf challenge

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,16 @@
 console.clear();
 
-const url = "https://swapi.dev/api/people";
+const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  console.log(data.results);
+  console.log(data.results[2].eye_color);
+  return data;
+}
+
+console.log();
 
 fetchData();
