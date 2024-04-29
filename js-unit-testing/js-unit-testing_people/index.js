93572b101365307e2728b79c716a4b3f9d6806f5
Janaina Voigt <jv.de@icloud.com>
2024-04-29 14:25:35 +0200

    Added the tests of the challenge people

--- js-unit-testing/js-unit-testing_people/index.js
+++ js-unit-testing/js-unit-testing_people/index.js
@@ -2,9 +2,13 @@ export function getFirstNames(people) {
   return people.map((person) => person.firstName);
 }
 
-export function getFullNames(people) {}
+export function getFullNames(people) {
+  return people.map((person) => person.firstName + " " + person.lastName);
+}
 
-export function getNameAndAge(people) {}
+export function getNameAndAge(people) {
+  return people.map((person) => `${person.lastName} (${person.age})`);
+}
 
 export function getPeopleByAge(people, age) {}
 
@@ -12,8 +16,12 @@ export function getPeopleNamesOlderThan(people, age) {}
 
 export function getPeopleByLastName(people, lastName) {}
 
-export function findPersonById(people, id) {}
+export function findPersonById(people, id) {
+  return people.find((person) => person.id === id);
+}
 
-export function isAnyoneOlderThan(people, age) {}
+export function isAnyoneOlderThan(people, age) {
+  return people.some((person) => person.age < age);
+}
 
 export function getFullNamesSortedByAge(people) {}
