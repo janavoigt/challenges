668d3328b85a1d8e7c6d7e17a03c841408ed8a00
Janaina Voigt <jv.de@icloud.com>
2024-04-29 12:02:00 +0200

    Added the test with divide()

--- js-unit-testing/js-unit-testing_math-functions-1/index.test.js
+++ js-unit-testing/js-unit-testing_math-functions-1/index.test.js
@@ -1,6 +1,7 @@
 import { add } from "./index.js";
 import { subtract } from "./index.js";
 import { multiply } from "./index.js";
+import { divide } from "./index.js";
 
 test("returns 5 if called add(2, 3))", () => {
   expect(add(2, 3)).toBe(5);
@@ -36,4 +37,12 @@ test("returns a negative value if only the second argument is negative", () => {
 
 test("returns a positive value if called with two negative arguments", () => {
   expect(multiply(-2, -4)).toBe(8);
+});
+
+test("returns 3 if called with divide(9, 3)", () => {
+  expect(divide(9, 3)).toBe(3);
+});
+
+test("returns 'You should not do this!' if called with 0 as second argument", () => {
+  expect(divide(9, 0)).toBe("You should not do this!");
 });
