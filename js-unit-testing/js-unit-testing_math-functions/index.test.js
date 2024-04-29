81d76f9375769f41330a82d9be89d92638727d73
Janaina Voigt <jv.de@icloud.com>
2024-04-29 11:15:14 +0200

    Added the challenge

---
+++ js-unit-testing/js-unit-testing_math-functions/index.test.js
@@ -0,0 +1,5 @@
+import { add } from "./index.js";
+
+// returns 5 if called with add(2, 3)
+//   - returns a negative value if the greater argument is negative (Hint: use the matcher `toBeLessThan()`)
+//   - returns a value close to 0.3 if called with add(0.1, 0.2) (Hint: use the matcher `toBeCloseTo()`)
