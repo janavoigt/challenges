36bc6fab78983e90df13385c595d1333ba834243
Janaina Voigt <jv.de@icloud.com>
2024-04-29 11:54:08 +0200

    Added the test with subtract()

--- js-unit-testing/js-unit-testing_math-functions-1/index.test.js
+++ js-unit-testing/js-unit-testing_math-functions-1/index.test.js
@@ -1,4 +1,5 @@
 import { add } from "./index.js";
+import { subtract } from "./index.js";
 
 test("returns 5 if called add(2, 3))", () => {
   expect(add(2, 3)).toBe(5);
@@ -12,4 +13,10 @@ test("returns a value close to 0.3 if called with add(0.1, 0.2)", () => {
   expect(add(0.1, 0.2)).toBeCloseTo(0.3);
 });
 
-//   - returns a value close to 0.3 if called with add(0.1, 0.2) (Hint: use the matcher `toBeCloseTo()`)
+test("returns 10 if called with subtract(15, 5)", () => {
+  expect(subtract(15, 5)).toBe(10);
+});
+
+test("returns a negative value if the second argument is greater than the first one", () => {
+  expect(subtract(2, 3)).toBeLessThan(0);
+});
