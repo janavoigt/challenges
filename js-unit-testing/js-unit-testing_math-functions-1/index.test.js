8433e962d6fc6033b324092b1f9dce28d83f601b
Janaina Voigt <jv.de@icloud.com>
2024-04-29 11:48:19 +0200

    Added the test with add()

--- js-unit-testing/js-unit-testing_math-functions-1/index.test.js
+++ js-unit-testing/js-unit-testing_math-functions-1/index.test.js
@@ -1,5 +1,15 @@
 import { add } from "./index.js";
 
-// returns 5 if called with add(2, 3)
-//   - returns a negative value if the greater argument is negative (Hint: use the matcher `toBeLessThan()`)
+test("returns 5 if called add(2, 3))", () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test("returns a negative value if the greater argument is add(-2, -3)", () => {
+  expect(add(-2, -3)).toBeLessThan(0);
+});
+
+test("returns a value close to 0.3 if called with add(0.1, 0.2)", () => {
+  expect(add(0.1, 0.2)).toBeCloseTo(0.3);
+});
+
 //   - returns a value close to 0.3 if called with add(0.1, 0.2) (Hint: use the matcher `toBeCloseTo()`)
