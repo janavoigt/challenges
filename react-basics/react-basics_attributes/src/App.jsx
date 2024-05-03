b639b9ed61b1838e015e6f943bcf245dde79ccdc
Janaina Voigt <jv.de@icloud.com>
2024-05-03 16:21:29 +0200

    Added the result

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return (
+    <div>
+      <Article />
+    </div>
+  );
+}
+
+export function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Friends Forever</h2>
+      <label htmlFor="(!)" id="(!)">
+        Search here
+      </label>
+      <input type="text" id="(!)" />
+      <a
+        href="https://en.wikipedia.org/wiki/Phoebe_Buffay"
+        className="article__link"
+      >
+        read here
+      </a>
+    </article>
+  );
 }
