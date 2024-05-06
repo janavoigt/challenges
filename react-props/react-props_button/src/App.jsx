42a19b945fb2ed737cb23425d4bcef6f19001c4f
Janaina Voigt <jv.de@icloud.com>
2024-05-06 16:56:36 +0200

    Added the function handleClick

--- react-props/react-props_button/src/App.jsx
+++ react-props/react-props_button/src/App.jsx
@@ -1,20 +1,24 @@
 import "./App.css";
 
 export default function App() {
+  function handleClick() {
+    console.log("meow");
+  }
   return (
     <h1>
       <Button
         style={{ color: "red", backgroundColor: "gray" }}
         type={"disabled"}
         text={"Click me!"}
+        onclick={handleClick}
       />
     </h1>
   );
 }
 
-function Button({ style, disabled, text }) {
+function Button({ style, disabled, text, onclick }) {
   return (
-    <button type={disabled} style={style}>
+    <button onClick={onclick} type={disabled} style={style}>
       {text}
     </button>
   );
