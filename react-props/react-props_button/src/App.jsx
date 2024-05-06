1696284b87976731d68566a54ba48912cb3f396c
Janaina Voigt <jv.de@icloud.com>
2024-05-06 16:27:44 +0200

    Added the first task

--- react-props/react-props_button/src/App.jsx
+++ react-props/react-props_button/src/App.jsx
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <h1>
+      <Button
+        style={{ color: "red", backgroundColor: "gray" }}
+        type={"disabled"}
+        text={"Click me!"}
+      />
+    </h1>
+  );
+}
+
+function Button({ style, disabled, text }) {
+  return (
+    <button type={disabled} style={style}>
+      {text}
+    </button>
+  );
 }
