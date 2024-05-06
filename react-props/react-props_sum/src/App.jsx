11c759c5b5b10c0eefb53dde9c6a1c1644cc1b30
Janaina Voigt <jv.de@icloud.com>
2024-05-06 11:59:14 +0200

    Install the npm i

--- react-props/react-props_sum/src/App.jsx
+++ react-props/react-props_sum/src/App.jsx
@@ -1,5 +1,13 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <h1>
+      <Sum valueA={3} valueB={2} />
+    </h1>
+  );
+}
+
+function Sum({ valueA, valueB }) {
+  return valueA + valueB;
 }
