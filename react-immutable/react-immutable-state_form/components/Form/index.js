801b82ae09dcc15cbab9ca37cefad981ae26a2b3
Janaina Voigt <jv.de@icloud.com>
2024-05-23 12:01:50 +0200

    Added the useImmer into the functions

--- react-immutable/react-immutable-state_form/components/Form/index.js
+++ react-immutable/react-immutable-state_form/components/Form/index.js
@@ -1,36 +1,42 @@
-import { useState } from "react";
 import { StyledForm, StyledInputContainer } from "./Form.styled";
+import { useImmer } from "use-immer";
 
 export default function Form() {
-  const [mountain, setMountain] = useState({
+  const [mountain, updateMountain] = useImmer({
     name: "Mount Everest",
     values: {
       altitude: 8848,
       mountainRange: "Himalayas",
     },
   });
-
   function handleNameChange(event) {
     event.preventDefault();
     let nameMountain = event.target.value;
-    setMountain({ ...mountain, name: nameMountain });
+    updateMountain((draft) => {
+      draft.name = nameMountain;
+    });
   }
+  // const handleNameChange = (event) => {
+  //   event.preventDefault();
+  //   let nameMountain = event.target.value;
+  //   updateMountain((draft) => {
+  //     draft.name = nameMountain;
+  //   });
+  // };
 
   function handleAltitudeChange(event) {
     event.preventDefault();
     let altitudeMountain = event.target.value;
-    setMountain({
-      ...mountain,
-      values: { ...mountain.values, altitude: altitudeMountain },
+    updateMountain((draft) => {
+      draft.values.altitude = altitudeMountain;
     });
   }
 
   function handleMountainRangeChange(event) {
     event.preventDefault();
     let mountainRanges = event.target.value;
-    setMountain({
-      ...mountain,
-      values: { ...mountain.values, mountainRange: mountainRanges },
+    updateMountain((draft) => {
+      draft.values.mountainRange = mountainRanges;
     });
   }
 
