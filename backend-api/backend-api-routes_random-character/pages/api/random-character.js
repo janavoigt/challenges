6af45127a2841c60e9f38406f9224958792b93ec
Janaina Voigt <jv.de@icloud.com>
2024-06-02 15:08:36 +0200

    Added the result of challenge

--- backend-api/backend-api-routes_random-character/pages/api/random-character.js
+++ backend-api/backend-api-routes_random-character/pages/api/random-character.js
@@ -1,12 +1,15 @@
-const character = {
-  firstName: chance.first(),
-  lastName: chance.last(),
-  age: chance.age(),
-  hobby: chance.hobby(),
-
-  // ... some more keys here ...
-};
+import Chance from "chance";
 
 export default function handler(req, res) {
+  const chance = new Chance();
+
+  const character = {
+    firstName: chance.first(),
+    lastName: chance.last(),
+    age: chance.age(),
+    twitter: chance.twitter(),
+    geohash: chance.geohash(),
+  };
+
   res.status(200).json(character);
 }
--- backend-api/backend-api-routes_random-character/pages/index.js
+++ backend-api/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,29 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error } = useSWR(
+    `http://localhost:3000/api/random-character`,
+    fetcher
+  );
+
+  if (!data) {
+    return;
+  }
+
+  if (error) return <div>Failed to Load your request</div>;
+
+  console.log(data);
+
+  return (
+    <>
+      <h2>
+        Hello, I am {data.firstName} {data.lastName}
+      </h2>
+
+      <p>Age: {data.age}</p>
+      <p>Twitter: {data.twitter}</p>
+      <p>Geohash: {data.geohash}</p>
+    </>
+  );
 }
