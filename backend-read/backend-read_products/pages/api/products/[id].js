8276bb727edfba81c106ca1ee7adab033e107544
Janaina Voigt <jv.de@icloud.com>
2024-06-03 11:53:04 +0200

    Added the first part of the challenge

--- backend-read/backend-read_products/pages/api/products/[id].js
+++ backend-read/backend-read_products/pages/api/products/[id].js
@@ -1,13 +1,21 @@
-import { products } from "../../../lib/products";
+import Product from "@/db/models/Products";
+import dbConnect from "@/db/models/dbConnect";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { id } = request.query;
+  await dbConnect();
 
-  const product = products.find((product) => product.id === id);
-
-  if (!product) {
-    return response.status(404).json({ status: "Not Found" });
+  if (request.method === "GET") {
+    const product = await Product.findById(id);
+    return response.status(200).json(product);
   }
-
-  response.status(200).json(product);
 }
+
+//   const product = products.find((product) => product.id === id);
+
+//   if (!product) {
+//     return response.status(404).json({ status: "Not Found" });
+//   }
+
+//   response.status(200).json(product);
+// }
