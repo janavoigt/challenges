1179772f9e67701f46ab28e9782873f9d23db6ad
Janaina Voigt <jv.de@icloud.com>
2024-06-17 10:21:57 +0200

    Added the challenge 17.06

---
+++ Kata/string.js
@@ -0,0 +1,18 @@
+// The main idea is to count all the occurring characters in a string.
+//If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {
+  const obj = {};
+
+  for (let i = 0; i < string.length; i++) {
+    if (obj.hasOwnProperty(string[i])) {
+      obj[string[i]] += 1;
+    } else {
+      obj[string[i]] = 1;
+    }
+  }
+  return obj;
+}
+count("janaina");
